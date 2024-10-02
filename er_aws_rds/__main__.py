99c79428a59865e85eb9a65bca9f8c52389879ba
Jordi Piriz <jpiriz@redhat.com>
2024-10-02 14:28:03 +0200

    improve main file

--- er_aws_rds/__main__.py
+++ er_aws_rds/__main__.py
@@ -1,12 +1,34 @@
+import os
+
 from cdktf import App
 from external_resources_io.input import parse_model, read_input_from_file
 
 from er_aws_rds.input import AppInterfaceInput
 from er_aws_rds.rds import Stack
 
-app_interface_input: AppInterfaceInput = parse_model(
-    AppInterfaceInput, read_input_from_file()
-)
-app = App()
-Stack(app, "CDKTF", app_interface_input)
-app.synth()
+
+def get_ai_input() -> AppInterfaceInput:
+    """Get the AppInterfaceInput from the input file."""
+    return parse_model(
+        AppInterfaceInput,
+        read_input_from_file(
+            file_path=os.environ.get("ER_INPUT_FILE", "/inputs/input.json"),
+        ),
+    )
+
+
+def init_cdktf_app(ai_input: AppInterfaceInput, id_: str = "CDKTF") -> App:
+    """Initialize the CDKTF app and all the stacks."""
+    app = App(outdir=os.environ.get("ER_OUTDIR", None))
+    Stack(app, id_, ai_input)
+    return app
+
+
+def main() -> None:
+    """Proper entry point for the CDKTF app."""
+    app = init_cdktf_app(get_ai_input())
+    app.synth()
+
+
+if __name__ == "__main__":
+    main()
